14c21b6dcc4df02657a1d713fe266f348e03e802
Patrick Baselier <patrick@kabisa.nl>
2015-06-03 17:20:56 +0200

    Add bootstrap

--- app/adapters/journalistiek.js
+++ app/adapters/journalistiek.js
@@ -6,7 +6,7 @@ export default DS.RESTAdapter.extend({
     var url = 'http://backstage-api.openstate.eu/v0/journalistiek/search',
       query = {
          query: "klokkenluiders",
-         size: 100
+         size: 5
       };
 
     return Ember.$.post(url, JSON.stringify(query), null, 'json');
